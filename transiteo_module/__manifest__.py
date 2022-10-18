b5428c72cba6a72dd1cd446b470041d4b2a795f7
MohammedBelhajWorkspace <114990508+MohammedBelhajWorkspace@users.noreply.github.com>
2022-10-18 16:57:28 +0200

    Update __manifest__.py

--- transiteo_module/__manifest__.py
+++ transiteo_module/__manifest__.py
@@ -9,7 +9,7 @@
         Thanks to transiteo's artificial intelligence, you can now classify your products into HS Tariff Codes automatically. Don't waste any more time or money getting this information.
     """,
     
-    # 'author': "transiteo, cross border solutions",
+    # 'author': "Mohammed Belhaj",
     'author': "transiteo, cross border solutions",
     'website': "https://transiteo.com/",
 
